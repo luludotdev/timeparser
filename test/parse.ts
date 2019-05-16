0ea04307e381705f5d22c8728b1d7feb45ba618a
Jack Baron <jackmbaron@gmail.com>
2019-05-16 03:26:05 +0100

    add simple short form tests

--- test/parse.ts
+++ test/parse.ts
@@ -1,6 +1,44 @@
 import test from 'ava'
+import { parse } from '../src/index'
+import { DAY, HOUR, MINUTE, MONTH, SECOND, WEEK, YEAR } from '../src/units'
 
-test.todo('parses simple short form')
+test('parses simple short form', t => {
+  // Seconds
+  t.is(parse('1s'), SECOND * 1)
+  t.is(parse('2s'), SECOND * 2)
+  t.is(parse('5s'), SECOND * 5)
+
+  // Minutes
+  t.is(parse('1m'), MINUTE * 1)
+  t.is(parse('2m'), MINUTE * 2)
+  t.is(parse('5m'), MINUTE * 5)
+
+  // Hours
+  t.is(parse('1h'), HOUR * 1)
+  t.is(parse('2h'), HOUR * 2)
+  t.is(parse('5h'), HOUR * 5)
+
+  // Days
+  t.is(parse('1d'), DAY * 1)
+  t.is(parse('2d'), DAY * 2)
+  t.is(parse('5d'), DAY * 5)
+
+  // Weeks
+  t.is(parse('1w'), WEEK * 1)
+  t.is(parse('2w'), WEEK * 2)
+  t.is(parse('5w'), WEEK * 5)
+
+  // Months
+  t.is(parse('1mo'), MONTH * 1)
+  t.is(parse('2mo'), MONTH * 2)
+  t.is(parse('5mo'), MONTH * 5)
+
+  // Years
+  t.is(parse('1y'), YEAR * 1)
+  t.is(parse('2y'), YEAR * 2)
+  t.is(parse('5y'), YEAR * 5)
+})
+
 test.todo('parses complex short form')
 test.todo('parses long form')
 test.todo('parses keywords')
