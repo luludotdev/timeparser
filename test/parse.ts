06e513130c2ec8d6cd109ba05eeefa31f663bb2f
Jack Baron <jackmbaron@gmail.com>
2019-05-16 03:19:00 +0100

    add test suite

---
+++ test/parse.ts
@@ -0,0 +1,7 @@
+import test from 'ava'
+
+test.todo('parses simple short form')
+test.todo('parses complex short form')
+test.todo('parses long form')
+test.todo('parses keywords')
+test.todo('fails on invalid input')
