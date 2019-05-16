d19668d066022336830c0400cc59184049889148
Jack Baron <jackmbaron@gmail.com>
2019-05-16 04:42:07 +0100

    unit resolver

--- src/units.ts
+++ src/units.ts
@@ -6,6 +6,41 @@ export const WEEK = DAY * 7
 export const MONTH = DAY * 31
 export const YEAR = DAY * 365
 
+export const resolveUnit = (str: string) => {
+  switch (str) {
+    case 's':
+    case 'second':
+    case 'seconds':
+      return SECOND
+    case 'm':
+    case 'minute':
+    case 'minutes':
+      return MINUTE
+    case 'h':
+    case 'hour':
+    case 'hours':
+      return HOUR
+    case 'd':
+    case 'day':
+    case 'days':
+      return DAY
+    case 'w':
+    case 'week':
+    case 'weeks':
+      return WEEK
+    case 'mo':
+    case 'month':
+    case 'months':
+      return MONTH
+    case 'y':
+    case 'year':
+    case 'years':
+      return YEAR
+    default:
+      return undefined
+  }
+}
+
 const UNITS = {
   DAY,
   HOUR,
